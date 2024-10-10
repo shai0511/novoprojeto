e8c03fb836d88c0b8bb9d943ab058da9b36c1278
shai0511 <shaine.paula@escola.pr.gov.br>
2024-10-10 09:37:38 -0300

    commit

--- pendulo.js
+++ pendulo.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 const length = 150; // comprimento do pêndulo
-let angle = Math.PI / 3; // ângulo inicial
+let angle = Math.PI / 5; // ângulo inicial
 let angleVelocity = 0.8; // velocidade angular
 let angleAcceleration = 0; // aceleração angular
 const damping = 0.990; // fator de amortecimento
